068e529923d0cfaad3a6db941e1d0fa2e3c320b9
Tanishka Verma <tanishkaverma423@gmail.com>
2026-08-03 21:46:38 +0530

    Time: 0 ms (100%), Space: 43.8 MB (8.73%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int l = digits.length;
+        for (int i = l-1; i >= 0; i--) {
+            if (digits[i] != 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+        int ans[] = new int[l + 1];
+        ans[0] = 1;
+        return ans;
+    }
+}
