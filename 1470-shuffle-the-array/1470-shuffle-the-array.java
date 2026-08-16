24506b15ba21f436c0e4407953555981b87bbc6c
Tanishka Verma <tanishkaverma423@gmail.com>
2026-08-16 17:25:37 +0530

    Time: 0 ms (100%), Space: 46.5 MB (62.99%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int ans[] = new int[2 * n];
+
+        for (int i = 0; i < 2 * n; i += 2) {
+            ans[i] = nums[i / 2];
+            ans[i + 1] = nums[i / 2 + n];
+        }
+        return ans;
+    }
+}
